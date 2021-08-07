3158127678d8e8965df8e07688654b98ecf0a86e
Marek Owsikowski <owsikowski@gmail.com>
2021-08-07 07:30:25 +0200

    Scripted auto-commit on change (2021-08-07 07:30:25) by gitwatch.sh

---
+++ fuck-git/fejs_bg.wasm.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function compile_to_ast(a: number, b: number): number;
+export function compile_to_lowered_ast(a: number, b: number): number;
+export function get_token(a: number, b: number): number;
+export function compile(a: number, b: number): number;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export function __wbindgen_exn_store(a: number): void;
