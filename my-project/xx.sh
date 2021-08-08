3434709ff630b0af4f4bd111b1554c0f782d149b
Marek Owsikowski <owsikowski@gmail.com>
2021-08-08 20:12:54 +0200

    Scripted auto-commit on change (2021-08-08 20:12:54) by gitwatch.sh

---
+++ my-project/xx.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#_(
+   #_DEPS is same format as deps.edn. Multiline is okay.
+   DEPS='
+   {:deps 
+   	{cli-matic {:mvn/version "0.3.3"}}}
+   '
+
+   #_You can put other options here
+   OPTS='
+   -J-Xms256m -J-Xmx256m 
+   -J-client
+   -J-Dclojure.spec.skip-macros=true
+   '
+exec clojure $OPTS -Sdeps "$DEPS" "$0" "$@"
+)
+
+(println "It works!")
+
+
