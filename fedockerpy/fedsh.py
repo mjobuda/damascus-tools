65c61b04878427e901c34cb6dbe70a638d531e30
Marek Owsikowski <owsikowski@gmail.com>
2021-08-11 05:29:52 +0200

    Scripted auto-commit on change (2021-08-11 05:29:52) by gitwatch.sh

--- fedockerpy/fedsh.py
+++ fedockerpy/fedsh.py
@@ -1,6 +1,5 @@
 import os
 
-os.system('docker run  -v `pwd`:`pwd` -w `pwd`   ekovege/fe if_statement.fe')
 
 def compile(commandOptions):
     return(os.system('docker run  -v `pwd`:`pwd` -w `pwd`   ekovege/fe '+commandOptions))
