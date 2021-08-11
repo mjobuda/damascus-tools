f1472afa188c955ff3d7e3ab53de3f743a400a53
Marek Owsikowski <owsikowski@gmail.com>
2021-08-11 03:36:37 +0200

    Scripted auto-commit on change (2021-08-11 03:36:37) by gitwatch.sh

--- fedockerjs/index.js
+++ fedockerjs/index.js
@@ -1,1 +1,11 @@
-dockerCLI.dockerCommand('run  -v /home/mmm/github/damascus-tools/fedockerjs:/home/mmm/github/damascus-tools/fedockerjs -w /home/mmm/github/damascus-tools/fedockerjs   ekovege/fe if_statement.fe').then(function (data) {console.log('data = ', data); });
+var dockerCLI = require("docker-cli-js");
+
+function compile(options) {
+  return dockerCLI
+    .dockerCommand(
+      "run  -v /home/mmm/github/damascus-tools/fedockerjs:/home/mmm/github/damascus-tools/fedockerjs -w /home/mmm/github/damascus-tools/fedockerjs   ekovege/fe ${ options }"
+    )
+    .then(function (data) {
+      console.log("data = ", data);
+    });
+}
