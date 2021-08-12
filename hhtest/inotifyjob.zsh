fbba4661e2b337adf3b89f3a3926ea28d02ab77d
Marek Owsikowski <owsikowski@gmail.com>
2021-08-12 10:20:00 +0200

    Scripted auto-commit on change (2021-08-12 10:20:00) by gitwatch.sh

--- hhtest/inotifyjob.zsh
+++ hhtest/inotifyjob.zsh
@@ -1,3 +1,3 @@
 while inotifywait -e modify .; do
-  cd ../hardhat-fe && npm runu build && cd ../hhtest && npx hardhat clean && npx hardhat compile
+  cd ../hardhat-feXX && npm run build && cd ../hhtest && npx hardhat clean && npx hardhat compile
 done
