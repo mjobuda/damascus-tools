7aa1d46bc64c6b1f8737c9c919b2b5c27fa1e9da
Marek Owsikowski <owsikowski@gmail.com>
2021-08-15 01:18:20 +0200

    Scripted auto-commit on change (2021-08-15 01:18:20) by gitwatch.sh

--- www.programmingfonts.org/fenanoidetag.js
+++ www.programmingfonts.org/fenanoidetag.js
@@ -23,6 +23,40 @@ contract Foo:
     }
   }
 );
+
+var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+  //var editor = CodeMirror(document.getElementById("code"), {
+  lineNumbers: true,
+  styleActiveLine: true,
+  matchBrackets: true,
+  lineWrapping: true,
+  indentUnit: 4,
+  lineWrapping: true,
+  value: `# The 'contract' keyword defines a new contract type
+      contract GuestBook:
+          # Strings are generic over a constant number
+          # that restricts its maximum size
+          messages: Map<address, String<100>>
+
+          # Events can be defined on contract or module level
+          event Signed:
+              book_msg: String<100>
+
+          pub def sign(book_msg: String<100>):
+              # All storage access is explicit using 'self.<some-key>'
+              self.messages[msg.sender] = book_msg
+
+              # Emit the 'Signed' event
+              emit Signed(book_msg=book_msg)
+
+          pub def get_msg(addr: address) -> String<100>:
+              # Copying data from storage to memory
+              # has to be done explicitly via 'to_mem()'
+              return self.messages[addr].to_mem()`,
+  mode: "ruby",
+});
+window.editor = editor;
+
 // <script type="module">
 // CodeMirror
 // var editor = CodeMirror(document.getElementById("CODEEDITOR"), {
