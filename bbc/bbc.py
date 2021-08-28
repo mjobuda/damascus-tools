2512f27149bf99ce815bae6c79075a1b10ee86cb
Marek Owsikowski <owsikowski@gmail.com>
2021-08-28 02:42:05 +0200

    Scripted auto-commit on change (2021-08-28 02:42:05) by gitwatch.sh

--- bbc/bbc.py
+++ bbc/bbc.py
@@ -6,7 +6,10 @@ from os.path import expanduser
 from os import system
 import sys
 
-nreplPort = sys.argv[1]
+if len(sys.argv) == 1:
+    nreplPort = 1667
+else:
+    nreplPort = sys.argv[1]
 system('bb nrepl-server '+nreplPort)
 
 myPromptSession = PromptSession(
