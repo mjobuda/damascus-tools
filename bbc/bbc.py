a232e4eec173c9579da80ff11f5ef8b26facbf20
Marek Owsikowski <owsikowski@gmail.com>
2021-08-28 02:33:58 +0200

    Scripted auto-commit on change (2021-08-28 02:33:58) by gitwatch.sh

--- bbc/bbc.py
+++ bbc/bbc.py
@@ -3,9 +3,14 @@ from prompt_toolkit import PromptSession
 from prompt_toolkit.history import FileHistory
 from os.path import expanduser
 
+from os import system
+
+system('bb nrepl-server')
+nreplPort = 1667  # todo
+
 myPromptSession = PromptSession(
-    history=FileHistory(expanduser('~/.myhistory')))
+    history=FileHistory(expanduser('~/.bbc_history')))
 
 while True:
-    userInput = myPromptSession.prompt('Enter command')
+    userInput = myPromptSession.prompt('Enter command:\n')
     print("{}, interesting.".format(userInput))
