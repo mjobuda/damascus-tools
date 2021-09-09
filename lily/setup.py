bb3a06bbba33a47169f777077f935d1360d679c9
Marek Owsikowski <owsikowski@gmail.com>
2021-09-10 00:52:20 +0200

    Scripted auto-commit on change (2021-09-10 00:52:20) by gitwatch.sh

--- lily/setup.py
+++ lily/setup.py
@@ -14,6 +14,6 @@ setup(
     packages=find_packages(),    
     install_requires=['ptpython >= 3.0.0', 'hissp'],
     entry_points={
-        'console_scripts': ['lily=lily:main']
+        'console_scripts': ['lily=lily:run']
     }
 )
