8f6abfe2dc03af718fc203aedddb940f1154aa31
Marek Owsikowski <owsikowski@gmail.com>
2021-09-10 01:42:15 +0200

    Scripted auto-commit on change (2021-09-10 01:42:15) by gitwatch.sh

--- lily/setup.py
+++ lily/setup.py
@@ -5,6 +5,16 @@
 import os
 import sys
 
+#who invented this bullshit??
+#python is such a nice language,
+#how could it happen that such a disgusting
+# completele unnecessary and bloated pile 
+# of garbage could become a setup tool?
+#I spent a few hours on this ceremony
+#what for?? 
+#a complete waste of time
+
+
 from setuptools import find_packages, setup
 
 #  with open(os.path.join(os.path.dirname(__file__), "README.rst")) as f:
